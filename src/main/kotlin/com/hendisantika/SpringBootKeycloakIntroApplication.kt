07e067023fec31a9b1d8ac44b94773e264cfb892
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-20 12:25:10 +0700

    #1 - Initial Commit

---
+++ src/main/kotlin/com/hendisantika/SpringBootKeycloakIntroApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKeycloakIntroApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKeycloakIntroApplication>(*args)
+}
