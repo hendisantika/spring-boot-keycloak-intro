6e452921ec7dc70a4b4b956b5145273ad9c84bdb
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-20 12:29:20 +0700

    #4 - Add WebSecurityConfig

---
+++ src/main/kotlin/com/hendisantika/config/WebSecurityConfig.kt
@@ -0,0 +1,31 @@
+package com.hendisantika.config
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-intro
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/01/22
+ * Time: 12.28
+ * To change this template use File | Settings | File Templates.
+ */
+@KeycloakConfiguration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+class WebSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
+
+    @Autowired
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        val keycloakAuthenticationProvider = keycloakAuthenticationProvider()
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(SimpleAuthorityMapper())
+        auth.authenticationProvider(keycloakAuthenticationProvider)
+    }
+
+}
