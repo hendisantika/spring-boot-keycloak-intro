5ea0bde95cb5829977c895bbac6c1f8799db3efd
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-20 12:32:25 +0700

    #9 - Add many APIs on ExampleController

--- src/main/kotlin/com/hendisantika/controller/ExampleController.kt
+++ src/main/kotlin/com/hendisantika/controller/ExampleController.kt
@@ -1,5 +1,8 @@
 package com.hendisantika.controller
 
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,4 +18,18 @@ import org.springframework.web.bind.annotation.RestController
  */
 @RestController
 @RequestMapping("/api")
-class ExampleController
+class ExampleController {
+    @GetMapping("/example")
+    @PreAuthorize("hasRole('USER')")
+    fun getUserInfo(): ResponseEntity<String> =
+        ResponseEntity.ok("User info")
+
+    @GetMapping("/example/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    fun getAdminInfo(): ResponseEntity<String> =
+        ResponseEntity.ok("User info")
+
+    @GetMapping("/public/example")
+    fun getPublicInfo(): ResponseEntity<String> =
+        ResponseEntity.ok("Public info")
+}
