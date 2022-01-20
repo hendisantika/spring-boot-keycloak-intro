706815d7f1573bf7d5bc71b2902ee3cd000ce163
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-20 12:31:19 +0700

    #8 - Add ExampleController

---
+++ src/main/kotlin/com/hendisantika/controller/ExampleController.kt
@@ -0,0 +1,18 @@
+package com.hendisantika.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-intro
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/01/22
+ * Time: 12.30
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api")
+class ExampleController
